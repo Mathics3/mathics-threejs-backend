6e3090fce7fc7c89b6c65ce6385c2f25f9dadf8a
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-07-04 19:15:32 -0300

    Improve non-triangular and non-coplanar polygons performance

--- src/primitives.js
+++ src/primitives.js
@@ -261,28 +261,28 @@ export default {
 						)
 					)
 				);
+
+				geometry.computeFaceNormals();
 			} else {
-				geometry = new Geometry();
+				geometry = new BufferGeometry();
 
-				const coordinates = [];
+				const coordinates = new Float32Array(coords.length * 3);
 
-				coords.forEach((coordinate) => {
-					coordinates.push(...coordinate[0]);
-					geometry.vertices.push(new Vector3(...coordinate[0]));
+				coords.forEach((coordinate, i) => {
+					coordinates[i * 3] = coordinate[0][0];
+					coordinates[i * 3 + 1] = coordinate[0][1];
+					coordinates[i * 3 + 2] = coordinate[0][2];
 				});
 
-				const triangles = earcut(coordinates);
+				geometry.setAttribute(
+					'position',
+					new BufferAttribute(coordinates, 3)
+				);
 
-				for (let i = 0; i < triangles.length; i += 3) {
-					geometry.faces.push(new Face3(
-						triangles[i],
-						triangles[i + 1],
-						triangles[i + 2]
-					));
-				}
+				geometry.setIndex(earcut(coordinates));
+
+				geometry.computeVertexNormals();
 			}
-
-			geometry.computeFaceNormals();
 		};
 
 		return new Mesh(geometry, new MeshLambertMaterial({
