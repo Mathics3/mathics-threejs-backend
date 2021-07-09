9beef394b124c73ee64e73c581a7c8c68b716ad2
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-07-09 20:54:31 -0300

    Add translation layer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import drawGraphics3d from './graphics3d.js';
 
-window.drawGraphics3d = drawGraphics3d;
+// it'll be removed on Mathics Core update
+window.drawGraphics3d = (object) => {
+	object.elements.forEach((primitive) => {
+		if (primitive.faceColor) {
+			primitive.color = primitive.faceColor;
+		}
+	});
+
+	object.lighting.forEach((light) => {
+		if (light.position) {
+			light.coords = [light.position];
+		}
+
+		light.type.toLowerCase();
+	});
+
+	return drawGraphics3d(object);
+};
 
 export default drawGraphics3d;
