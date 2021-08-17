c246cb771ef8d5cf11257d8562c8b226761de675
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-08-17 13:56:54 -0300

    Add `edgeForm` to cylinders

--- src/primitives.js
+++ src/primitives.js
@@ -212,50 +212,334 @@ export default {
 
 		return cuboids;
 	},
-	cylinder: ({ color, coords, opacity = 1, radius }, extent) => {
-		const cylinders = new InstancedMesh(
-			new CylinderGeometry(radius, radius, 1, 24)
-				.translate(0, -0.5, 0) // translate the geometry so we don't need to calculate the middle of each coordinates-pair
-				.rotateX(Math.PI / 2), // rotate the cylinder 90 degrees to lookAt work
-			new MeshLambertMaterial({
-				color: new Color(...color).getHex(),
-				opacity,
-				transparent: opacity !== 1,
-				depthWrite: opacity === 1
-			}),
-			coords.length / 2
-		);
+	cylinder: ({ color, coords, edgeForm = {}, opacity = 1, radius = 1 }, extent) => {
+		// number of vertex per coordinate / number of coordinates per cylinder = 3 / 2
+		const cylindersBegin = new Float32Array(coords.length * 1.5);
+		const cylindersEnd = new Float32Array(coords.length * 1.5);
 
 		for (let i = 0; i < coords.length / 2; i++) {
-			const startCoordinate = new Vector3(
-				...(coords[i * 2][0] ?? scaleCoordinate(coords[i * 2][1], extent))
-			);
+			const startCoordinate = coords[i * 2][0] ?? scaleCoordinate(coords[i * 2][1], extent);
 
-			const endCoordinate = new Vector3(
-				...(coords[i * 2 + 1][0] ?? scaleCoordinate(coords[i * 2 + 1][1], extent))
-			);
+			const endCoordinate = coords[i * 2 + 1][0] ?? scaleCoordinate(coords[i * 2 + 1][1], extent);
 
-			cylinders.setMatrixAt(
-				i,
-				new Matrix4()
-					.setPosition(startCoordinate)
-					.lookAt(
-						startCoordinate,
-						endCoordinate,
-						cylinders.up
-					)
-					.scale(
-						new Vector3(
-							1,
-							1,
-							// the height of the cylinder
-							startCoordinate.distanceTo(endCoordinate),
-						)
-					)
-			);
-		};
+			cylindersBegin[i * 3] = startCoordinate[0];
+			cylindersBegin[i * 3 + 1] = startCoordinate[1];
+			cylindersBegin[i * 3 + 2] = startCoordinate[2];
 
-		return cylinders;
+			cylindersEnd[i * 3] = endCoordinate[0];
+			cylindersEnd[i * 3 + 1] = endCoordinate[1];
+			cylindersEnd[i * 3 + 2] = endCoordinate[2];
+		}
+
+		const cylinderGeometry = new InstancedBufferGeometry().copy(
+			new CylinderGeometry(radius, radius, 1, 24)
+				.translate(0, -0.5, 0) // translate the geometry so we don't need to calculate the middle of each coordinates-pair
+				.rotateX(Math.PI / 2) // rotate the cylinder 90 degrees to lookAt work
+		);
+
+		cylinderGeometry.instanceCount = coords.length / 2;
+
+		cylinderGeometry.setAttribute(
+			'cylinderBegin',
+			new InstancedBufferAttribute(cylindersBegin, 3)
+		);
+
+		cylinderGeometry.setAttribute(
+			'cylinderEnd',
+			new InstancedBufferAttribute(cylindersEnd, 3)
+		);
+
+		const cylinders = new Mesh(
+			cylinderGeometry,
+			new ShaderMaterial({
+				transparent: opacity !== 1,
+				depthWrite: opacity === 1,
+				lights: true,
+				uniforms: {
+					...UniformsLib.lights,
+					diffuse: { value: color },
+					opacity: { value: opacity },
+				},
+				vertexShader: `
+					attribute vec3 cylinderBegin;
+					attribute vec3 cylinderEnd;
+
+					varying vec3 vLightFront;
+					varying vec3 vIndirectFront;
+
+					#include <common>
+					#include <lights_pars_begin>
+
+					void main() {
+						#include <beginnormal_vertex>
+						#include <defaultnormal_vertex>
+
+						vec3 z = normalize(cylinderBegin - cylinderEnd);
+						// if z.z is 0 the cylinder doesn't appear
+						z.z += 0.0001;
+
+						vec3 x = normalize(cross(vec3(0, 1, 0), z));
+						vec3 y = cross(z, x);
+
+						float height = distance(cylinderBegin, cylinderEnd);
+
+						// position, rotate and scale the cylinder
+						mat4 cylinderMatrix = mat4(x, 0, y, 0, z * height, 0, cylinderBegin, 1);
+
+						vec4 mvPosition = modelViewMatrix * cylinderMatrix * vec4(position, 1);
+
+						gl_Position = projectionMatrix * mvPosition;
+
+						#include <lights_lambert_vertex>
+					}
+				`,
+				fragmentShader: `
+					uniform vec3 diffuse;
+					uniform float opacity;
+
+					varying vec3 vLightFront;
+					varying vec3 vIndirectFront;
+
+					#include <common>
+					#include <bsdfs>
+
+					void main() {
+						ReflectedLight reflectedLight = ReflectedLight(vec3(0.0), vec3(0.0), vec3(0.0), vec3(0.0));
+
+						reflectedLight.indirectDiffuse = vIndirectFront * BRDF_Diffuse_Lambert(diffuse);
+
+						reflectedLight.directDiffuse = vLightFront * BRDF_Diffuse_Lambert(diffuse);
+
+						gl_FragColor = vec4(reflectedLight.directDiffuse + reflectedLight.indirectDiffuse, opacity);
+					}
+				`
+			})
+		);
+
+		// without this the cylinders disappear when the zoom is big
+		cylinders.frustumCulled = false;
+
+		if (edgeForm.showEdges === false) {
+			// if the edges aren't shown the work is done
+			return cylinders;
+		}
+
+		const group = new Group();
+
+		group.add(cylinders);
+
+		// differently from cuboid's edges, the cylinders's ones are in a different object. It is very hard or maybe impossible to draw edges with complex shapes in the fragment shader
+
+		const edgesGeometry = new InstancedBufferGeometry()
+			.setAttribute(
+				'position',
+				new BufferAttribute(
+					new Float32Array([
+						0, -1, 0,
+						0.258819043636322, -0.9659258127212524, 0,
+
+						0.258819043636322, -0.9659258127212524, 0,
+						0.5, -0.8660253882408142, 0,
+
+						0.5, -0.8660253882408142, 0,
+						0.7071067690849304, -0.7071067690849304, 0,
+
+						0.7071067690849304, -0.7071067690849304, 0,
+						0.8660253882408142, -0.5, 0,
+
+						0.8660253882408142, -0.5, 0,
+						0.9659258127212524, -0.258819043636322, 0,
+
+						0.9659258127212524, -0.258819043636322, 0,
+						1, 0, 0,
+
+						1, 0, 0,
+						0.9659258127212524, 0.258819043636322, 0,
+
+						0.9659258127212524, 0.258819043636322, 0,
+						0.8660253882408142, 0.5, 0,
+
+						0.8660253882408142, 0.5, 0,
+						0.7071067690849304, 0.7071067690849304, 0,
+
+						0.7071067690849304, 0.7071067690849304, 0,
+						0.5, 0.8660253882408142, 0,
+
+						0.5, 0.8660253882408142, 0,
+						0.258819043636322, 0.9659258127212524, 0,
+
+						0.258819043636322, 0.9659258127212524, 0,
+						0, 1, 0,
+
+						0, 1, 0,
+						-0.258819043636322, 0.9659258127212524, 0,
+
+						-0.258819043636322, 0.9659258127212524, 0,
+						-0.5, 0.8660253882408142, 0,
+
+						-0.5, 0.8660253882408142, 0,
+						-0.7071067690849304, 0.7071067690849304, 0,
+
+						-0.7071067690849304, 0.7071067690849304, 0,
+						-0.8660253882408142, 0.5, 0,
+
+						-0.8660253882408142, 0.5, 0,
+						-0.9659258127212524, 0.258819043636322, 0,
+
+						-0.9659258127212524, 0.258819043636322, 0,
+						-1, 0, 0,
+
+						-1, 0, 0,
+						-0.9659258127212524, -0.258819043636322, 0,
+
+						-0.9659258127212524, -0.258819043636322, 0,
+						-0.8660253882408142, -0.5, 0,
+
+						-0.8660253882408142, -0.5, 0,
+						-0.7071067690849304, -0.7071067690849304, 0,
+
+						-0.7071067690849304, -0.7071067690849304, 0,
+						-0.5, -0.8660253882408142, 0,
+
+						-0.5, -0.8660253882408142, 0,
+						-0.258819043636322, -0.9659258127212524, 0,
+
+						-0.258819043636322, -0.9659258127212524, 0,
+						0, -1, 0,
+
+						0.258819043636322, -0.9659258127212524, -1,
+						0, -1, -1,
+
+						0.5, -0.8660253882408142, -1,
+						0.258819043636322, -0.9659258127212524, -1,
+
+						0.7071067690849304, -0.7071067690849304, -1,
+						0.5, -0.8660253882408142, -1,
+
+						0.8660253882408142, -0.5, -1,
+
+						0.7071067690849304, -0.7071067690849304, -1,
+						0.9659258127212524, -0.258819043636322, -1,
+
+						0.8660253882408142, -0.5, -1,
+
+						1, 0, -1,
+						0.9659258127212524, -0.258819043636322, -1,
+
+						0.9659258127212524, 0.258819043636322, -1,
+						1, 0, -1,
+
+						0.8660253882408142, 0.5, -1,
+						0.9659258127212524, 0.258819043636322, -1,
+
+						0.7071067690849304, 0.7071067690849304, -1,
+						0.8660253882408142, 0.5, -1,
+
+						0.5, 0.8660253882408142, -1,
+						0.7071067690849304, 0.7071067690849304, -1,
+
+						0.258819043636322, 0.9659258127212524, -1,
+						0.5, 0.8660253882408142, -1,
+
+						0, 1, -1,
+						0.258819043636322, 0.9659258127212524, -1,
+
+						-0.258819043636322, 0.9659258127212524, -1,
+						0, 1, -1,
+
+						-0.5, 0.8660253882408142, -1,
+						-0.258819043636322, 0.9659258127212524, -1,
+
+						-0.7071067690849304, 0.7071067690849304, -1,
+						-0.5, 0.8660253882408142, -1,
+
+						-0.8660253882408142, 0.5, -1,
+						-0.7071067690849304, 0.7071067690849304, -1,
+
+						-0.9659258127212524, 0.258819043636322, -1,
+						-0.8660253882408142, 0.5, -1,
+
+						-1, 0, -1,
+						-0.9659258127212524, 0.258819043636322, -1,
+
+						-0.9659258127212524, -0.258819043636322, -1,
+						-1, 0, -1,
+
+						-0.8660253882408142, -0.5, -1,
+						-0.9659258127212524, -0.258819043636322, -1,
+
+						-0.7071067690849304, -0.7071067690849304, -1,
+						-0.8660253882408142, -0.5, -1,
+
+						-0.5, -0.8660253882408142, -1,
+						-0.7071067690849304, -0.7071067690849304, -1,
+
+						-0.258819043636322, -0.9659258127212524, -1,
+						-0.5, -0.8660253882408142, -1,
+
+						0, -1, -1,
+						-0.258819043636322, -0.9659258127212524, -1
+					]),
+					3
+				)
+			)
+			// scaling z changes the position of the edges
+			.scale(radius, radius, 1);
+
+		edgesGeometry.instanceCount = coords.length / 2;
+
+		edgesGeometry.setAttribute(
+			'cylinderBegin',
+			new InstancedBufferAttribute(cylindersBegin, 3)
+		);
+
+		edgesGeometry.setAttribute(
+			'cylinderEnd',
+			new InstancedBufferAttribute(cylindersEnd, 3)
+		);
+
+		const edges = new LineSegments(
+			edgesGeometry,
+			new ShaderMaterial({
+				uniforms: {
+					color: { value: edgeForm.color ?? [0, 0, 0] }
+				},
+				vertexShader: `
+					attribute vec3 cylinderBegin;
+					attribute vec3 cylinderEnd;
+
+					void main() {
+						vec3 z = normalize(cylinderBegin - cylinderEnd);
+						// if z.z is 0 the edges doesn't appear
+						z.z += 0.0001;
+
+						vec3 x = normalize(cross(vec3(0.0, 1.0, 0.0), z));
+						vec3 y = cross(z, x);
+
+						float height = distance(cylinderBegin, cylinderEnd);
+
+						// position, rotate and scale the cylinder
+						mat4 cylinderMatrix = mat4(x, 0, y, 0, z * height, 0, cylinderBegin, 1);
+
+						gl_Position = projectionMatrix * modelViewMatrix * cylinderMatrix * vec4(position, 1.0);
+					}
+				`,
+				fragmentShader: `
+					uniform vec3 color;
+
+					void main() {
+						gl_FragColor = vec4(color, 1.0);
+					}
+				`
+			})
+		);
+
+		// without this the edges disappear when the zoom is big
+		edges.frustumCulled = false;
+
+		group.add(edges);
+
+		return group;
 	},
 	line: ({ color, coords, opacity = 1 }, extent) => {
 		const coordinates = new Float32Array(coords.length * 3);
@@ -1197,7 +1481,7 @@ export default {
 
 		group.add(polyhedrons);
 
-		// differently from cuboid's edges, the polyhedron's ones are in a different object. It is very hard or maybe impossible to draw edges with complex shapes in the fragment shader
+		// the polyhedrons' edges are basicaly the same as the cylinders' ones
 
 		const edgesGeometry = new InstancedBufferGeometry().copy(
 			new EdgesGeometry(polyhedronGeometry) // "calculate" the edges of the desired polyhedron
