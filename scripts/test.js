1f8ac2046646eda1e157fc670cd6c32687917063
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2021-09-16 20:32:42 -0300

    Remove duplicated error when tests fail

--- scripts/test.js
+++ scripts/test.js
@@ -5,8 +5,16 @@ const app = express();
 
 app.use(express.static(__dirname + '/../'));
 
-const server = app.listen(8080, async () => {
-	await backstop('test');
+const server = app.listen(8080, () => {
+	backstop('test')
+		.then(() => {
+			server.close();
+		})
+		.catch(() => {
+			// We catch the error to NodeJS don't show it (BackstopJS already does that)
+			server.close();
 
-	server.close();
+			// If the exit code be 0 (what it is by default), the tests are going to pass in GitHub Actions.
+			process.exit(1);
+		})
 });
