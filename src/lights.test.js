9286963056472ddca9dac8e2534a7ef8644da6c0
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-02-28 01:20:12 +0000

    Move tests to Jest

---
+++ src/lights.test.js
@@ -0,0 +1,6 @@
+import { testScreenshot } from '../jest.utils.js';
+
+testScreenshot('Test ambient light', 'ambient_light.html');
+testScreenshot('Test directional light', 'directional_light.html');
+testScreenshot('Test point light', 'point_light.html');
+testScreenshot('Test spot light', 'spot_light.html');
---
+++ src/primitives/line.test.js
@@ -0,0 +1,4 @@
+import { testScreenshot } from '../../jest.utils.js';
+
+testScreenshot('Test non-dashed line', 'line.html');
+testScreenshot('Test dashed line', 'dashed_line.html');
